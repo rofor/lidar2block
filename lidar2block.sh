21a4c1e63c809773137e1e467736ede68ae6c6ac
rofor <rofor@protonmail.com>
2017-09-03 19:19:45 +0100

    clear

--- lidar2block.sh
+++ lidar2block.sh
@@ -6,7 +6,7 @@
 # meshlab
 
 ################################################## prep ###############################################
-  /usr/bin/rm -rf  block.xyz ;# delete old block file
+  rm -rf -R temp ;# delete old temp directory
   mkdir temp # make new temp directory
   size=$(head -n 1 st8991_DTM_1M.asc | grep -o '[0-9]\+')
   sed -e '1,6d'  $1 > temp/matrix.xyz # strip ersi headers
@@ -16,31 +16,25 @@
       sed 's/\xllcorner.*/xllcorner          1/' $1 > temp/xllcorner.asc # set xllcorner to 1
       sed 's/\yllcorner.*/yllcorner          1/' temp/xllcorner.asc > temp/yllcorner.asc # set yllcorner to 1
   		gdal_translate -of XYZ temp/yllcorner.asc temp/0-terrain.xyz # generate point cloud from raster
-  	#	awk '{print $1, $2, $3/$size}' temp/cloud.xyz > temp/ # divide z axis by 100 to generate terrain point cloud
-    #  cat temp/cdp.xyz | sed -re 's/([0-9]+\.[0-9]{2})[0-9]+/\1/g' > temp/0-terrain.xyz # cull all by two decimal places
   echo "Done."
 
 ##################################### extract terrain z for each face #################################
 echo "Extracting Z values"
 # north terrain z
   head -n1 temp/matrix.xyz > temp/fr.xyz # print first row of matrix #
-    tr -s ' '  '\n' < temp/fr.xyz > temp/ntz.xyz # transpose row to column
-    #    awk '{print $1/$size}' temp/ntzm.xyz > temp/ntzf.xyz # scale
-    #      awk -F. '{print $1}' temp/ntzf.xyz > temp/ntz.xyz # cull float
+    tr -s ' '  '\n' < temp/fr.xyz > temp/ntzf.xyz # transpose row to column
+          awk -F. '{print $1}' temp/ntzf.xyz > temp/ntz.xyz # cull float
 # east terrain z
   awk '{print $(NF-1)}' temp/matrix.xyz > temp/etzm.xyz # print second from last column of matrix
-    #  awk '{print $1/$size}' temp/etzm.xyz > temp/etzf.xyz # scale
-    #    awk -F. '{print $1}' temp/etzf.xyz > temp/etzr.xyz # cull float
-          awk '{a[i++]=$0} END {for (j=i-1; j>=0;) print a[j--] }' temp/etzm.xyz  > temp/etz.xyz # reverse list
+        awk -F. '{print $1}' temp/etzm.xyz > temp/etzr.xyz # cull float
+          awk '{a[i++]=$0} END {for (j=i-1; j>=0;) print a[j--] }' temp/etzr.xyz  > temp/etz.xyz # reverse list
 # south terrain z
     tail -n1 temp/matrix.xyz > temp/lr.xyz # print last row of matrix
-      tr -s ' '  '\n' < temp/lr.xyz > temp/stz.xyz # transpose row to column
-      #    awk '{print $1/$size}' temp/stzm.xyz > temp/stzf.xyz # scale
-      #      awk -F. '{print $1}' temp/stzf.xyz > temp/stz.xyz # cull float
+      tr -s ' '  '\n' < temp/lr.xyz > temp/stzf.xyz # transpose row to column
+            awk -F. '{print $1}' temp/stzf.xyz > temp/stz.xyz # cull float
 # west terrain z
-    awk '{print $1}' temp/matrix.xyz > temp/wtzm.xyz # print first column of matrix
-#        awk '{print $1/$size}' temp/wtzm.xyz > temp/wtzf.xyz # scale
-  #        awk -F. '{print $1}' temp/wtzf.xyz > temp/wtzr.xyz # cull float
+    awk '{print $1}' temp/matrix.xyz > temp/wtzf.xyz # print first column of matrix
+          awk -F. '{print $1}' temp/wtzf.xyz > temp/wtzm.xyz # cull float
             awk '{a[i++]=$0} END {for (j=i-1; j>=0;) print a[j--] }' temp/wtzm.xyz > temp/wtz.xyz # reverse list
 echo "Done."
 ############################################### generate faces #######################################
